f7c74b1cc19b561017a089f121b4b47b2696dba6
gideon0088 <rogelio.bajo@gmail.com>
2026-05-10 22:23:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+import express from "express";
+import fetch from "node-fetch";
 
+const app = express();
+
+const API_URL = "YOUR_APPS_SCRIPT_URL";
+
+app.get("/api", async (req, res) => {
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  res.json(data);
+});
+
+app.listen(3000, () => console.log("Server running"));
