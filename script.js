6561270413293428ad37db2f253f0e36861510f4
gideon0088 <rogelio.bajo@gmail.com>
2026-05-10 22:31:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-fetch("/api")
-  .then(res => res.json())
-  .then(data => {
-    document.body.innerHTML = "<h1>Craft World 🚀</h1>";
-
-    data.forEach(item => {
-      document.body.innerHTML += `
-        <div>
-          <strong>${item.Resource}</strong> - ${item.Profit}
-        </div>
-      `;
-    });
-  });
+alert("script is running");
